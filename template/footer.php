33b5c5016bdad5ef4f44ab0e2bf7b7c4be08b2de
thesspyy <49035521+thesspyy@users.noreply.github.com>
2020-02-15 16:05:37 +0700

    commit_header and footer_1

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+
+
+  <footer class="footer mt-auto py-3 text-center fixed-bottom"  style="background-color:#F5F5F5;">
+    <div class="container">
+      <span class="text-muted">ลิขสิทธิ์ © 2563 Department of Computer Science. สงวนลิขสิทธิ์.</span>
+    </div>
+  </footer>
+  </body>
+
+</html
