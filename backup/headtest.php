3906c7e897d158a199deed95fd66f93f083eafa4
inwwei <weijatupon@gmail.com>
2020-03-03 17:01:38 +0700

    commit update and back up

---
+++ backup/headtest.php
@@ -0,0 +1,95 @@
+<?php 
+    include "controller.php";
+    $action = new Action();
+    session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Carousel Template · Bootstrap</title>
+
+    <link rel="stylesheet" type="text/css" href="UI.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+</head>
+
+<body >
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top  grad">
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <a class="py-1" href="#" aria-label="Product">
+                    <a href="#" class="navbar-brand d-flex align-items-center ">
+                        <img src="logo/logo_sc.png" width="55" class="mr-3">
+                    </a>
+                </a>
+            </div>
+            <h2 style="font-size:30px" class="h2">หน้าหลัก</h2>&nbsp;&nbsp;&nbsp;&nbsp;
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a class="btext" href="#">ยืมอุปกรณ์</a>
+                    </li>&nbsp;&nbsp;
+                    <li class="nav-item">
+                        <a class="btext" href="#">แจ้งซ่อม</a>
+                    </li>&nbsp;&nbsp;
+                    <li class="nav-item">
+                        <a class="btext" href="#">ตรวจสอบสถานะการซ่อม</a>
+                    </li><br>
+                </ul>
+                <!-- profile ค่อยมาแก้เป็นดึงรูปคนสมัครจากดาต้าเบสเอา-->
+                <div class="profile">
+                    <a href="#" class="navbar-brand d-flex align-items-center">
+                        <img src="image/msi.jpg" width="55" class="mr-3">
+                    </a>
+                </div>&nbsp;&nbsp;
+
+                <button class="btn btn-outline-dark my-2 my-sm-0" type="submit" href="#">
+                    sign in
+                </button>&nbsp;&nbsp;
+                <button class="btn btn-outline-dark my-2 my-sm-0" type="submit" href="#">
+                    sign up
+                </button>
+
+            </div>
+        </nav>
+
+    </header>
+    <div class="position-relative overflow-hidden">
+        <h3>รายการอุปกรณ์</h3>
+    </div>
+    <br><br>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script>
+    window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')
+    </script>
+    <script src="/docs/4.4/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
